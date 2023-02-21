21c591519fe4a15f8d761a21e5f53c362c00c38d
Kevin Williams <75566465+KevinWilliams68@users.noreply.github.com>
2023-02-20 19:22:56 -0800

    Update 70_Climbing_Stairs.py

--- 70_Climbing_Stairs.py
+++ 70_Climbing_Stairs.py
@@ -1,6 +1,7 @@
-def climbStairs(n):
+def climbStairs(self,n):
     if n == 1:
         return 1
     elif n == 2:
         return 2
-    return (climbStairs(n-2) + climbStairs(n-1))
+    return (self.climbStairs(n-2) + self.climbStairs(n-1))
+#takes a very long time
