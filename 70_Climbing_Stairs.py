46e7a61b8a09277c6f6c8b3c26858b6a66893dcb
Kevin Williams <75566465+KevinWilliams68@users.noreply.github.com>
2024-01-10 13:14:21 -0800

    Update 70_Climbing_Stairs.py

--- 70_Climbing_Stairs.py
+++ 70_Climbing_Stairs.py
@@ -5,3 +5,12 @@ def climbStairs(self,n):
         return 2
     return (self.climbStairs(n-2) + self.climbStairs(n-1))
 #takes a very long time
+
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 0, 1
+        for i in range(n):
+	        a, b = b, a + b
+        return b
